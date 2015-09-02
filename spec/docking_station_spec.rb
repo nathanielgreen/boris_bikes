1fa1e3a6022c2fca53e6876c5453bf592ed1b2d3
Nathaniel Green <nathanielgreen@Nathaniels-MacBook-Pro.local>
2015-09-02 17:02:10 +0100

    Added the respond_to for the dock method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,13 @@ require 'docking_station'
 
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
+    it { is_expected.to respond_to :release_bike }
 
-  it 'releases a bike' do
-    bike = subject.release_bike
-    expect(bike).to respond_to :working?
-  end
+    it 'releases a bike' do
+      bike = subject.release_bike
+      expect(bike).to respond_to :working?
+    end
+
+    it  {is_expected.to respond_to(:dock).with(1).argument }
 
   end
