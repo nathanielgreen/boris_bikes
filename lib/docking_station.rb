654523260dadb24c5b956becfb37475193d23d84
Nathaniel Green <nathanielgreen@Nathaniels-MacBook-Pro.local>
2015-09-02 14:29:36 +0100

    Modified Docking Station and Bike, & Specs"

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,6 @@
 class DockingStation
+  
   def release_bike
-    
+    Bike.new
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,12 @@
 require 'docking_station'
 
+
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
+
+  it 'releases a bike' do
+    bike = subject.release_bike
+    expect(bike).to respond_to :working?
+  end
 
   end
